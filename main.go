8b601809d895cddf35023d2fd685b251720e0739
Renilson <rjss3@aluno.ifal.edu.br>
2021-09-11 19:25:33 +0000

    Orientação sobre como utilizar a calculadora na URL

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
     //1 - Impementar o comando "go build && ./calc-server" no terminal (sem as aspas);
     //2 - Selecionar a opção abrir no navegador (open in browser);
     //3 - Inserir, no fim da URL, "soma/'valor1'/'valor2'" (sem as aspas);
-    //4 - O resultado da soma de "valor 1" e "valor 2" será exibido.
+    //4 - O resultado da soma de "valor 1" e "valor 2" será exibid.
 
 func main(){
 	app := fiber.New()
