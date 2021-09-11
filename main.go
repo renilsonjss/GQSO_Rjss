a1f6dd4148fa89f98aa7f01e312251e52ed23578
Renilson <rjss3@aluno.ifal.edu.br>
2021-09-11 19:45:34 +0000

    Mensagem inicial (orientação ao usuário)

--- main.go
+++ main.go
@@ -4,12 +4,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-//Para utilização da funcionalidade soma no navegador:
-    //1 - Impementar o comando "go build && ./calc-server" no terminal (sem as aspas);
-    //2 - Selecionar a opção abrir no navegador (open in browser);
-    //3 - Inserir, no fim da URL, "soma/'valor1'/'valor2'" (sem as aspas);
-    //4 - O resultado da soma de "valor 1" e "valor 2" será exibid.
-
 func main(){
 	app := fiber.New()
 
