39a911f5001aeee9252ba357246d6d4f227bf54c
Renilson <rjss3@aluno.ifal.edu.br>
2021-09-11 19:36:39 +0000

    Mensagem inicial

--- main.go
+++ main.go
@@ -14,7 +14,11 @@ func main(){
 	app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Calculadora")
+        return c.SendString("Para utilização da funcionalidade soma no navegador:
+        1 - Impementar o comando 'go build && ./calc-server' no terminal (sem as aspas);
+        2 - Selecionar a opção abrir no navegador (open in browser);
+        3 - Inserir, no fim da URL, 'soma/valor1/valor2 (sem as aspas);
+        4 - O resultado da soma de 'valor 1' e 'valor 2' será exibida.")
     })
 
 	app.Get("/soma/:valor1/:valor2", soma)
